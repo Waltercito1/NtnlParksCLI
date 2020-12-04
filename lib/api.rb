507acbb2fe11b96699e11e3f9f6fbc8eca3009ba
Walter Aab <aabwalter@yahoo.com>
2020-12-04 11:20:28 -0600

    Add get_parks method to api.rb

--- lib/api.rb
+++ lib/api.rb
@@ -1,13 +1,18 @@
 require_relative '../config/environment'
 
-BASE_URL = "https://developer.nps.gov/api/v1/parks"
-park_code = "?api_key=" #"?parkCode=acad&api_key="
-KEY = "#{ENV['API_KEY']}"
+class API
+    BASE_URL = "https://developer.nps.gov/api/v1/parks"
+    KEY = "#{ENV['API_KEY']}"
 
-uri = URI(BASE_URL + park_code + KEY) 
-response = Net::HTTP.get(uri)
-formatted_resp = JSON.parse(response)
+    def get_parks
 
-data = formatted_resp["data"]
+        @park_code = "?api_key=" #"?parkCode=acad&api_key="
 
-binding.pry
+        uri = URI(BASE_URL + park_code + KEY) 
+        response = Net::HTTP.get(uri)
+        formatted_resp = JSON.parse(response)
+
+        data = formatted_resp["data"]
+    end
+    binding.pry
+end
