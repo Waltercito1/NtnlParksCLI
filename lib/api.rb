ba6a523872db2ae85709555937d09ee54ce35376
Walter Aab <aabwalter@yahoo.com>
2020-12-04 11:12:56 -0600

    Add nps.rb which will be used to start the CLI

--- lib/api.rb
+++ lib/api.rb
@@ -2,12 +2,12 @@ require_relative '../config/environment'
 
 BASE_URL = "https://developer.nps.gov/api/v1/parks"
 park_code = "?api_key=" #"?parkCode=acad&api_key="
-key = "#{ENV['API_KEY']}"
+KEY = "#{ENV['API_KEY']}"
 
-uri = URI(BASE_URL + park_code + key) 
+uri = URI(BASE_URL + park_code + KEY) 
 response = Net::HTTP.get(uri)
 formatted_resp = JSON.parse(response)
 
 data = formatted_resp["data"]
 
-#binding.pry
+binding.pry
