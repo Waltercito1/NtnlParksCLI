bc7e432992d2b0e9015bd409a80680be8ec72f78
Walter Aab <aabwalter@yahoo.com>
2020-12-11 18:49:40 -0600

    Add delete comments

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,6 @@ require 'dotenv/load'
 require 'colorize'
 #require 'io/console'
 
-require_relative '../lib/concerns/paramable'
 require_relative '../lib/api'
 require_relative '../lib/cli'
 require_relative '../lib/park'
