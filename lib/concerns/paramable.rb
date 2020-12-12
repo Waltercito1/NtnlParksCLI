824f90b549b6d33655f8cf109f20e9f556ecc814
Walter Aab <aabwalter@yahoo.com>
2020-12-11 18:45:48 -0600

    Add final touches before submit

--- lib/concerns/paramable.rb
+++
@@ -1,7 +0,0 @@
-module Paramable
-    # def message_2
-    #     puts "The U.S. is home to an astonishing 421 national parks,"
-    #     puts " monuments and nationally protected lands comprising the "
-    #     puts "vast National Park Service (NPS) system.\n"
-    # end
-end
